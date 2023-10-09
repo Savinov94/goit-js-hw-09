179735e8b3f678d18d22254a35dc8eeb67cf9922
Savinov94 <134193164+Savinov94@users.noreply.github.com>
2023-10-09 09:36:37 +0200

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,43 +1,46 @@
-function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.form');
+  const delayInput = form.querySelector('input[name="delay"]');
+  const stepInput = form.querySelector('input[name="step"]');
+  const amountInput = form.querySelector('input[name="amount"]');
 
-    setTimeout(() => {
-      const promiseResult = { position, delay };
+  async function createPromise(position, delay) {
+    return new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
 
-      if (shouldResolve) {
-        resolve(promiseResult);
-      } else {
-        reject(promiseResult);
-      }
-    }, delay);
-  });
-}
+      setTimeout(() => {
+        const promiseResult = { position, delay };
 
-const form = document.querySelector('.form');
-const delayInput = form.querySelector('input[name="delay"]');
-const stepInput = form.querySelector('input[name="step"]');
-const amountInput = form.querySelector('input[name="amount"]');
-
-form.addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  const delay = parseInt(delayInput.value);
-  const step = parseInt(stepInput.value);
-  const amount = parseInt(amountInput.value);
-
-  if (isNaN(delay) || isNaN(step) || isNaN(amount) || delay < 0 || step < 0 || amount <= 0) {
-    alert('Please enter valid values for delay, step, and amount.');
-    return;
-  }
-
-  for (let i = 1; i <= amount; i++) {
-    createPromise(i, delay).then(({ position, delay }) => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    }).catch(({ position, delay }) => {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        if (shouldResolve) {
+          resolve(promiseResult);
+        } else {
+          reject(promiseResult);
+        }
+      }, delay);
     });
-
-    delay += step;
   }
+
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    let delay = parseInt(delayInput.value);
+    const step = parseInt(stepInput.value);
+    const amount = parseInt(amountInput.value);
+
+    if (isNaN(delay) || isNaN(step) || isNaN(amount) || delay < 0 || step < 0 || amount <= 0) {
+      alert('Please enter valid values for delay, step, and amount.');
+      return;
+    }
+
+    for (let i = 1; i <= amount; i++) {
+      try {
+        const result = await createPromise(i, delay);
+        console.log(`✅ Fulfilled promise ${result.position} in ${result.delay}ms`);
+      } catch (error) {
+        console.log(`❌ Rejected promise ${error.position} in ${error.delay}ms`);
+      }
+
+      delay += step;
+    }
+  });
 });
